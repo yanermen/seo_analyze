2d9da5f71c2ffe659c98cfbb2b569824d72f4eb1
yanermen <yanermen@gmail.com>
2020-06-27 18:19:32 +0400

    scrapy_spider

--- seo_script/social_networks/social_networks/spiders/social_networks_spider.py
+++ seo_script/social_networks/social_networks/spiders/social_networks_spider.py
@@ -1,37 +1,25 @@
 import scrapy
-from scrapy.linkextractors.lxmlhtml import LxmlLinkExtractor
+import re
+
 
 class SocialNetworkSpider(scrapy.Spider):
     name = 'social_networks'
-    # allowed_domains = ['tripadvisor.com/']
-    allowed_domains = ['awca.com/']
-    # start_urls = ['https://www.tripadvisor.com/']
-    start_urls = ['https://www.awca.com/']
+    start_urls = ['https://www.mercedes-benz.com/']
 
-    #
-    # def parse(self, response):
-    #     social_networks_link = LxmlLinkExtractor(allow=r'https://[a-z.]+/[a-z.]+$',
-    #                                              deny_domains=['shop.github.com','youtube.com','twitter.com'], unique = True).extract_links(response)
-    #     for link in social_networks_link:
-    #         print(f'link.url', 'link.text')
-
-        # facebook_link = response.css('a::attr(href)').getall()
-        # facebook_link = response.xpath('//a/@href').getall()
-        # print(facebook_link)
 
     def parse(self, response):
-        """ Main function that parses downloaded pages """
-        # Print what the spider is doing
-        print(response.url)
-        # Get all the <a> tags
-        a_selectors = response.xpath("//a")
-        # Loop on each tag
-        for selector in a_selectors:
-            # Extract the link text
-            text = selector.xpath("text()").extract_first()
-            # Extract the link href
-            link = selector.xpath("@href").extract_first()
-            # Create a new Request object
-            request = response.follow(link, callback=self.parse)
-            # Return it thanks to a generator
-            yield request
+
+        social_networks_link = response.css('a::attr(href)').getall()
+
+        pattern_fb = r'^https://www.facebook.com/\w+'
+        pattern_twitter = r'^https://twitter.com/\w+'
+
+        for link in social_networks_link:
+            if re.match(pattern_fb, link):
+                print(link)
+                continue
+            elif re.match(pattern_twitter, link):
+                print(link)
+                break
+
+
